94966d2b7f84f300d7ae80c4b8e871bad1e4ca97
ABISHEK KSP <kspcsk14@gmail.com>
2026-05-10 15:25:57 +0530

    Fixed CORS policy and ensured /api prefix in paths

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ const adminRoutes = require('./routes/admin');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Routes
